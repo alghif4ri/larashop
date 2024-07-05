f4541a5caea94ab1c940b8ffd282412ee19b8a29
LuckyNoobz <r.abudzar@gmail.com>
2024-07-06 03:04:49 +0700

    re route login

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
 
 Route::livewire('/admin/product', 'product.index')
     ->name('admin.product')
