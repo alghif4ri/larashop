cfa8a2f6fda7d9dde34c92f73826cbeff9969a69
LuckyNoobz <r.abudzar@gmail.com>
2024-07-05 13:57:10 +0700

    remove item from cart

--- resources/views/livewire/shop/cart.blade.php
+++ resources/views/livewire/shop/cart.blade.php
@@ -13,22 +13,35 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($cart['products'] as $product)
+                            @if (!$cart['products'])
                                 <tr>
-                                    <td>{{ $product->title }}</td>
-                                    <td>Rp{{ number_format($product->price, 2, ',', '.') }}</td>
-                                    <td>
-                                        <button class="btn btn-sm btn-danger">
-                                            <i class="bi bi-trash"></i>
-                                        </button>
-                                    </td>
+                                    <td colspan="3" class="table-active text-center text-muted">Your Cart is Empty</td>
                                 </tr>
-                            @endforeach
+                            @else
+                                @foreach ($cart['products'] as $product)
+                                    <tr>
+                                        <td>{{ $product->title }}</td>
+                                        <td>Rp{{ number_format($product->price, 2, ',', '.') }}</td>
+                                        <td>
+                                            <button wire:click="removeFromCart({{ $product->id }})"
+                                                class="btn btn-sm btn-danger">
+                                                <i class="bi bi-trash"></i>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            @endif
                         </tbody>
                         <tfoot>
                             <tr>
-                                <td colspan="3"><button class="btn btn-primary float-right"><i
-                                            class="bi bi-bag-check"></i></button></td>
+                                @if (!$cart['products'])
+                                    <td colspan="3"><a href="{{ route('shop.index') }}"
+                                            class="btn btn-primary float-right">Shop <i class="bi bi-bag-check"></i></a>
+                                    </td>
+                                @else
+                                    <td colspan="3"><button class="btn btn-primary float-right">Checkout <i
+                                                class="bi bi-bag-check"></i></button></td>
+                                @endif
                             </tr>
                         </tfoot>
                     </table>
